0239e4388828263f67579eced7be19f9d4fad3a7
Gabriel Hutchison <gabehwebsites@gmail.com>
2018-05-26 14:50:41 -0400

    Tweak styles for story icons

--- src/StoryIcon.js
+++ src/StoryIcon.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import './StoryIcon.css';
 
 export default function StoryIcon(props) {
   return (
-    <div>
-      <FontAwesomeIcon icon={props.icon} className="large-icon" />
+    <div className="storyIcon">
+      <div className="iconContainer">
+        <FontAwesomeIcon icon={props.icon} className="large-icon" />
+      </div>
       <p>{props.content}</p>
     </div>
   );
