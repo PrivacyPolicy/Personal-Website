e4416f636967c3e91380112f7849f68bca78c788
Gabriel Hutchison <gabehwebsites@gmail.com>
2018-05-26 12:21:09 -0400

    Cleaned up story page

--- src/CenteredTable.js
+++ src/CenteredTable.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import CenteredTableCell from './CenteredTableCell';
 
 
 
 export default function CenteredTable(props) {
   return (
     <div className={`centered ${ props["className"] || "" }`}>
-      {
-        React.Children.map(props.children, (cell) => CenteredTableCell({children: cell}))
-      }
+      { props.children }
     </div>
   );
 }
--- src/CenteredTableCell.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function CenteredTable(props) {
-  return (
-    <div style={{marginLeft: "auto", marginRight: "auto"}}>
-      {props.children}
-    </div>
-  );
-}
