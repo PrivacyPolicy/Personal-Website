9fd4e02ec02d86e243150bb1de5248a70941574f
Gabriel Hutchison <gabehwebsites@gmail.com>
2018-06-07 16:52:00 -0400

    Update content and styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import ContactIcon from './ContactIcon.js';
 import StoryIcon from './StoryIcon.js';
 import CenteredTable from './CenteredTable.js';
 import Project from './Project.js';
-import { javaIcon, androidIcon, pythonIcon } from './TechIcons';
+import { javaIcon, androidIcon, pythonIcon, htmlIcon, cssIcon, jsIcon, phpIcon,
+  iosIcon, nodeIcon, ubuntuIcon, reactIcon } from './tech-icons';
 
 class App extends Component {
   render() {
@@ -17,13 +18,13 @@ class App extends Component {
           <CenteredTable>
             <div>
               <img src={profile} style={{borderRadius: "50%", width: 300}} alt="My Chiseled Face"></img>
-              <h2>Hi, I'm <span className="name">Gabriel Hutchison</span></h2>
-              <h3>Full-Stack Developer | Web Nerd | Non-Stop Creator</h3>
+              <h2 className="name">Hi, I'm <span>Gabriel Hutchison</span></h2>
+              <h3>Full-Stack Developer | Non-Stop Creator</h3>
               <div>
                 <ContactIcon href="https://www.github.com/PrivacyPolicy/" title="GitHub" icon={ faGithubSquare } />
                 <ContactIcon href="https://www.linkedin.com/in/gabriel-hutchison/" title="LinkedIn" icon={ faLinkedin } />
-                <ContactIcon href="mailto:gabehwebsites@gmail.com" title="gabehwebsites@gmail.com" icon={ faEnvelope } />
-                <ContactIcon href="http://www.gabeh.info/resume/" title="Resume" icon={ faFile } />
+                <ContactIcon href="mailto:gabehwebsites@gmail.com" title="GabeHwebsites@gmail.com" icon={ faEnvelope } />
+                <ContactIcon href="http://www.gabeh.info/resume/Gabriel%20Hutchison%27s%20Resume.pdf" title="Resume" icon={ faFile } />
               </div>
             </div>
           </CenteredTable>
@@ -31,56 +32,50 @@ class App extends Component {
 
         <section>
           <CenteredTable className="responsive">
-            <StoryIcon icon={ faBook } content="Full-stack Dev | Web Nerd" />
-            <StoryIcon icon={ faDesktop } content="Full-stack Dev | Web Nerd" />
-            <StoryIcon icon={ faCode } content="Full-stack Dev | Web Nerd" />
+            <StoryIcon icon={ faBook } content="
+              It all started when I grabbed a book on HTML4 back in 2012.
+              I made a few websites in my spare time, and I just never stopped
+              creating. One AWS internship later, and I've solidified my
+              love of software engineering.
+              " />
+            <StoryIcon icon={ faDesktop } content="
+              I code tons of random applications — sometimes desktop and mobile
+              apps, but mainly web apps. This website is one of them!
+              " />
+            <StoryIcon icon={ faCode } content="
+              I work best with Java, JavaScript, and Python. I also know a lot of
+              other backend languages and frontend frameworks, like Node.js and
+              React.js.
+              " />
           </CenteredTable>
         </section>
 
         <section className="projects">
           <h1 className="title">Projects</h1>
           <Project
-            title="Cool Cool"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                        sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia deserunt mollit anim id est laborum."
-            technologies={[javaIcon, androidIcon]}
-            demoLink="a"
-            codeLink="b"
-            image="https://cdn.lynda.com/course/506926/506926-636238695730179167-16x9.jpg" />
+            title="PolyHacks Website"
+            description="I developed the website and the iOS app for my school's
+              hackathon: PolyHacks. I was a dev team member for SHAPE, the
+              school's hackathon club."
+            technologies={[htmlIcon, cssIcon, jsIcon, phpIcon, iosIcon]}
+            demoLink="http://www.polyhacks.com"
+            codeLink="https://github.com/PrivacyPolicy/PolyHacks-com"
+            image="http://www.gabeh.info/projects/polyhacks.png" />
           <Project
-            title="Cool Cool"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                        sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia deserunt mollit anim id est laborum."
-            technologies={[androidIcon, pythonIcon]}
-            demoLink="a"
-            codeLink="b"
-            image="https://d2118lkw40i39g.cloudfront.net/wp-content/uploads/2016/01/maxresdefault-4.jpg" />
+            title="Autonomous Vehicle"
+            description="A 1/10-scale, level 3 autonomous vehicle based on MIT RACECAR for
+              my Autonomous Vehicles class."
+            technologies={[pythonIcon, ubuntuIcon]}
+            codeLink="https://github.com/Brent-Kuhn/Dat_Whip"
+            image="http://www.gabeh.info/projects/autonomous-vehicle.jpg" />
           <Project
-            title="Project Mandarin"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore magna
-                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit
-                        esse cillum dolore eu fugiat nulla pariatur. Excepteur
-                        sint occaecat cupidatat non proident, sunt in culpa qui
-                        officia deserunt mollit anim id est laborum."
-            technologies={[javaIcon, androidIcon]}
-            codeLink="b"
-            infoLink="c"
-            image="https://i.ytimg.com/vi/OfIQW6s1-ew/maxresdefault.jpg" />
+            title="Class Schedule Generator Web App"
+            description="A web app to generate a list of all possible
+              course selections that match a student's requirements. The course data
+              was scraped from the school's website with a Python scraper."
+            technologies={[htmlIcon, cssIcon, jsIcon, pythonIcon]}
+            codeLink="https://github.com/PrivacyPolicy/FloridaPolyScheduleGenerator"
+            image="http://www.gabeh.info/projects/schedule.png" />
         </section>
         <footer>
           Created with React.js by Gabriel Hutchison
---
+++ src/tech-icons.js
@@ -0,0 +1,57 @@
+import { faJava, faAndroid, faPython, faHtml5, faCss3Alt, faJsSquare, faPhp,
+  faAppStoreIos, faNodeJs, faLinux, faReact } from '@fortawesome/fontawesome-free-brands';
+
+export const javaIcon = {
+  icon: faJava,
+  title: "Java",
+};
+
+export const androidIcon = {
+  icon: faAndroid,
+  title: "Android",
+};
+
+export const pythonIcon = {
+  icon: faPython,
+  title: "Python",
+};
+
+export const htmlIcon = {
+  icon: faHtml5,
+  title: "HTML5",
+};
+
+export const cssIcon = {
+  icon: faCss3Alt,
+  title: "CSS3",
+};
+
+export const jsIcon = {
+  icon: faJsSquare,
+  title: "JavaScript",
+};
+
+export const phpIcon = {
+  icon: faPhp,
+  title: "PHP",
+};
+
+export const iosIcon = {
+  icon: faAppStoreIos,
+  title: "iOS",
+};
+
+export const nodeIcon = {
+  icon: faNodeJs,
+  title: "Node.js",
+}
+
+export const ubuntuIcon = {
+  icon: faLinux,
+  title: "Ubuntu",
+}
+
+export const reactIcon = {
+  icon: faReact,
+  title: "React.js",
+}
